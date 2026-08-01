4b7a96a80657efaa48958e1d304e37afe2da6adc
Zainisrar <zainisrar2003@gmail.com>
2026-08-01 14:04:03 +0500

    Remove matplotlib/geopandas dependencies to fix build issues

--- api/brd.py
+++ api/brd.py
@@ -12,11 +12,6 @@ import certifi
 import io
 import tempfile
 import json
-import matplotlib
-matplotlib.use("Agg")
-import matplotlib.pyplot as plt
-import geopandas as gpd
-import contextily as ctx
 import cloudinary
 import cloudinary.uploader
 from dotenv import load_dotenv
