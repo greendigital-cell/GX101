69e4bf3e1460fcf5f1455a896b27c8b91f4aea16
Zainisrar <zainisrar2003@gmail.com>
2026-08-06 22:40:29 +0500

    pushed

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from api.brd import router as project_sites_router
 from api.auth import router as auth_router
 from api.penpot import router as penpot_router
 from api.normalize import router as normalize_router
+from api.requirement import router as requirement_router
 
 app = FastAPI()
 
@@ -20,6 +21,7 @@ app.include_router(auth_router, prefix="/api/auth", tags=["authentication"])
 app.include_router(project_sites_router, prefix="/api/project-sites", tags=["project-sites"])
 app.include_router(penpot_router, prefix="/api/project-sites", tags=["penpot"])
 app.include_router(normalize_router, prefix="/api/normalize", tags=["normalize"])
+app.include_router(requirement_router, prefix="/api/requirements", tags=["requirements"])
 
 @app.get("/")
 async def root():
